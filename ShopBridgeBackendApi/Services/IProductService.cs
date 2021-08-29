8bc59c2daa758c24fb2ee347e70f02f6f47466ec
missdreamer <vashisht07july@gmail.com>
2021-08-29 18:34:54 +0530

    Add files via upload

---
+++ ShopBridgeBackendApi/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using ShopBridgeBackendApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridgeBackendApi.Services
+{
+    interface IProductService
+    {
+        void AddProduct( Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int id);
+    }
+}
---
+++ ShopBridgeBackendApi/Services/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using ShopBridgeBackendApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridgeBackendApi.Model
+{
+    public class ProductService:IProductService
+    {
+        //crud operation on db
+        private readonly ProductDbContext _context;
+        
+        public ProductService(ProductDbContext productDbContext)
+        {
+            _context = productDbContext;
+        }
+
+        public void AddProduct( Product product)
+        {
+
+            
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+           
+            _context.Products.Add(product);
+            
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+               
+            }
+           
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList<Product>();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+           
+        }
+        public Product GetProduct(int Id)
+        {
+            return _context.Products.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
